4164f44276446bc3914a5aee4762597fd39b2d46
Renee Messersmith <morganrmessersmith@gmail.com>
2024-11-13 15:20:04 -0500

    feat: live search functionality

--- src/App/App.js
+++ src/App/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import searchIcon from '../icons/search.png'
 import SubContainer from '../SubContainer/SubContainer.js'
 import { useEffect, useState } from 'react';
 
 function App() {
   const [selectedSub, setSelectedSub] = useState(null);
   const [subscriptions, setSubscriptions] = useState([]);
+  const [allSubscriptions, setAllSubscriptions] = useState([]);
+  const [subSearch, setSubSearch] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:3001/api/v1/subscriptions")
     .then(response => response.json())
-    .then(data => {setSubscriptions(data.data)})
+    .then(data => {setSubscriptions(data.data); setAllSubscriptions(data.data)})
   },[]);
 
   function showSubDetails(sub) {
@@ -42,10 +45,38 @@ function App() {
     });
   };
 
+  function searchSubList(event) {
+    const search = event.target.value
+    setSubSearch(search);
+    if (search === "") {
+      setSubscriptions(allSubscriptions)
+    } else {
+      const filteredSubs = allSubscriptions.filter(sub => {
+        console.log(sub.attributes.title, "searching here")
+        return sub.attributes.title.toLowerCase().includes(search.toLowerCase())
+      })
+      setSubscriptions(filteredSubs)
+    }   
+  };
+
+  const searchBar = (
+    <form onSubmit={(event) => event.preventDefault()}>
+      <img className='search-icon' src={searchIcon} alt='search icon'/>
+      <input
+        type="search"
+        id="site-search"
+        placeholder='  Start typing here...'
+        value={subSearch}
+        onChange={searchSubList}
+      />
+    </form>)
+
+
   return (
     <main className="App">
       <header className="App-header">
         <h1 onClick={(() => {showTeaSubs()})}>Tea Time</h1>
+        {searchBar}
       </header>
         <SubContainer
           subscriptions={subscriptions}
